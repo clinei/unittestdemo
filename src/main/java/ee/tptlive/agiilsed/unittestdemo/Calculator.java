2a7c746071742ab4fb0bf7f59f0c35caeaf75c06
dmitri.maksimov <dmitri.maksimov@tptlive.ee>
2018-04-10 16:44:18 +0300

    Removed redundant code.

--- src/main/java/ee/tptlive/agiilsed/unittestdemo/Calculator.java
+++ src/main/java/ee/tptlive/agiilsed/unittestdemo/Calculator.java
@@ -1,10 +1,5 @@
 package ee.tptlive.agiilsed.unittestdemo;
 
-import java.util.*;
-
-import static java.util.Arrays.asList;
-import static java.util.Collections.*;
-
 public class Calculator {
 
     public int evaluate(String input) {
@@ -17,33 +12,11 @@ public class Calculator {
         for (String number : numbers) {
             sum += Integer.parseInt(number);
         }
+        return sum;
 
 //        return Arrays.stream(numbers)
 //                .mapToInt(Integer::parseInt)
 //                .sum();
-
-        final Optional<String> name = getName();
-        System.out.println(name.map(String::length).orElseThrow(IllegalStateException::new));
-
-        System.out.println(getListOfNames().size());
-
-        Optional.ofNullable(getListOfNames());
-        return sum;
     }
 
-    public Optional<String> getName() {
-        if (new Random().nextBoolean()) {
-            return Optional.of("Dmitri");
-        } else {
-            return Optional.empty();
-        }
-    }
-
-    public List<String> getListOfNames() {
-        if (new Random().nextBoolean()) {
-            return asList("1", "2", "3");
-        } else {
-            return emptyList();
-        }
-    }
 }
