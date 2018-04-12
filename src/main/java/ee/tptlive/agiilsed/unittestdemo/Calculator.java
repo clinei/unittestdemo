99bdadab23ac7b7d5260e072c6fe92ab5731c64c
clinei <tanel58@hotmail.com>
2018-04-12 19:12:51 +0300

    more work towards parsing

--- src/main/java/ee/tptlive/agiilsed/unittestdemo/Calculator.java
+++ src/main/java/ee/tptlive/agiilsed/unittestdemo/Calculator.java
@@ -1,81 +1,45 @@
 package ee.tptlive.agiilsed.unittestdemo;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Calculator {
 
-    public abstract class Ast_Node {
+    private String input;
+    private int index;
+    private int x;
 
-        abstract public int evaluate();
+    private Map<Character, Integer> operator_precedences;
+
+    public Calculator() {
+
+        this.operator_precedences = new HashMap<Character, Integer>();
+
+        this.operator_precedences.put('+', 1);
+        this.operator_precedences.put('-', 1);
+        this.operator_precedences.put('*', 2);
+        this.operator_precedences.put('/', 2);
     }
 
-    // @Incomplete
-    // Variables
+    public int evaluate(String input) {
 
-    public class Ast_Literal extends Ast_Node {
-
-        public int value;
-
-        Ast_Literal(int value) {
-
-            this.value = value;
-        }
-
-        public int evaluate() {
-
-            return this.value;
-        }
+        return evaluate(input, 0);
     }
-
-    public class Ast_Operation extends Ast_Node {
-
-        Ast_Node left;
-        Ast_Node right;
-        String operation_type;
-
-        Ast_Operation(Ast_Node left, Ast_Node right, String operation_type) {
-
-            this.left = left;
-            this.right = right;
-            this.operation_type = operation_type;
-        }
-
-        public int evaluate() {
-
-            int left_result = this.left.evaluate();
-            int right_result = this.right.evaluate();
-
-            if (this.operation_type.equals("+")) {
-
-                return left_result + right_result;
-            }
-            if (this.operation_type.equals("-")) {
-
-                return left_result - right_result;
-            }
-            if (this.operation_type.equals("*")) {
-
-                return left_result * right_result;
-            }
-            if (this.operation_type.equals("/")) {
-
-                return left_result / right_result;
-            }
-        }
-    }
-
-    public Ast_Node parse(String input) {
-
-        for (int i = 0; i < input.length(); i += 1) {}
-
-        return new Ast_Literal(42);
-    }
-
-    public int evaluate(String input) {return evaluate(input, 0);}
 
     public int evaluate(String input, int x) {
 
         if (input == null || input.isEmpty()) {
+
             return 0;
         }
+
+        this.input = input;
+        this.index = -1;
+        this.x = x;
+
+        return parse_expression();
+
+        /*
 
         int sum = 0;
 
@@ -101,8 +65,110 @@ public class Calculator {
         }
 
         return sum;
+        */
+
 //        return Arrays.stream(numbers)
 //                .mapToInt(Integer::parseInt)
 //                .sum();
+    }
+
+    private char peek_next_token() {
+
+        if (index + 1 < input.length()) {
+
+            return input.charAt(index + 1);
+        }
+        else {
+
+            return '\0';
+        }
+    }
+
+    private int parse_primary() {
+
+        char lookahead = peek_next_token();
+
+        if (lookahead == 'x' || lookahead == 'X') {
+
+            return this.x;
+        }
+
+        String str = "";
+
+        if (lookahead == '-') {
+
+            str += '-';
+
+            this.index += 1;
+
+            lookahead = peek_next_token();
+        }
+
+        while (lookahead != '+' && lookahead != '-' &&
+               lookahead != '*' && lookahead != '/' &&
+               lookahead != 'x' && lookahead != '\0') {
+
+            str += lookahead;
+
+            this.index += 1;
+
+            lookahead = peek_next_token();
+        }
+
+        return Integer.parseInt(str);
+    }
+
+    private int parse_expression() {
+
+        return parse_expression_1(parse_primary(), 0);
+    }
+
+    private int parse_expression_1(int lhs, int min_precedence) {
+
+        char lookahead = peek_next_token();
+
+        Integer prec = this.operator_precedences.get(lookahead);
+
+        if (prec == null) {
+
+            return lhs;
+        }
+
+        while (prec >= min_precedence) {
+
+            char op = lookahead;
+
+            this.index += 1;
+
+            int rhs = parse_primary();
+
+            lookahead = peek_next_token();
+
+            while (this.operator_precedences.get(lookahead) > this.operator_precedences.get(op)) {
+
+                rhs = parse_expression_1(rhs, this.operator_precedences.get(lookahead));
+
+                lookahead = peek_next_token();
+            }
+
+            if (op == '+') {
+
+                lhs = lhs + rhs;
+            }
+            else if (op == '-') {
+
+                lhs = lhs - rhs;
+            }
+            else if (op == '*') {
+
+                lhs = lhs * rhs;
+            }
+            else if (op == '/') {
+
+                lhs = lhs / rhs;
+            }
+        }
+
+        return lhs;
     }
 }
